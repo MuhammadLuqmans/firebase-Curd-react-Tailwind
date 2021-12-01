c970cc927f446dfc40c050afbbf56bd3bda2a71e
MuhammadLuqmans <musa60902@gmail.com>
2021-12-01 13:14:21 +0500

    Firebase Crud

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAw7jTEQZTUBd5uxqRM6PazqFqZZhiQvBQ",
-  authDomain: "firechat-7f34f.firebaseapp.com",
-  projectId: "firechat-7f34f",
-  storageBucket: "firechat-7f34f.appspot.com",
-  messagingSenderId: "566617098812",
-  appId: "1:566617098812:web:cb0d23fc1d8b8de66bd139",
-};
+// firechat ahmad Muhammad luqman
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
